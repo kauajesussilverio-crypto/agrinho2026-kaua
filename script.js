a17cf524379508c1d1b0df69788c90784e688827
kauajesussilverio-crypto <kaua.jesus.silverio@escola.pr.gov.br>
2026-06-11 15:47:23 -0300

    agrinho

---
+++ script.js
@@ -0,0 +1,7 @@
+// Função para interagir com o usuário
+function mostrarMensagem() {
+    alert("Obrigado pelo interesse! Juntos, podemos construir uma agricultura muito mais verde e sustentável. Continue navegando para saber como!");
+}// Função para interagir com o usuário
+function mostrarMensagem() {
+    alert("Obrigado pelo interesse! Juntos, podemos construir uma agricultura muito mais verde e sustentável. Continue navegando para saber como!");
+}
